12f8dfac7d52dc7b27ef763106503f027337dc16
Tara <tara_polli@yahoo.com>
2021-03-26 13:30:35 -0400

    level-2 added

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,8 @@
+// Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends 
+//    a table to your web page and then adds new rows of data for each UFO sighting.
+
+// from data.js
+var tableData = data;
+
+
+
